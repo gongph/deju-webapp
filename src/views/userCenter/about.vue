a0b316a14a61c1280499d9d6853bb7124195b8a4
gongph <gongph@foxmail.com>
2019-03-06 11:43:44 +0800

    fix: 修改微信号跳转

--- src/views/userCenter/about.vue
+++ src/views/userCenter/about.vue
@@ -23,12 +23,14 @@
     <div class="empty-data__wrapper" v-else>
       <div class="list-item">
         <div class="label">微信公众号</div>
-        <div class="text">dejujinrong</div>
+        <div class="text">
+          <a target="_blank" href="https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzU3NDgxNDA2Nw==#wechat_redirect" class="telphone">渤源资本</a>
+        </div>
       </div>
       <div class="list-item">
         <div class="label">客服电话</div>
         <div class="text">
-          <a href="tel: 400-100-1234" class="telphone">400-100-1234</a>
+          <a href="tel: 18234208881" class="telphone">18234208881</a>
         </div>
       </div>
       <div class="list-item">
